f6b9351e2c2e1f49f4f7e85b32da95ad96f18f50
Huy <s3777280@s00101013n-29.rmit.edu.vn>
2022-01-10 17:18:47 +0700

    feat: finish problem 1

---
+++ samplefinaltest/src/com/finaltest/CourseList.java
@@ -0,0 +1,2 @@
+package com.finaltest;public class CourseList {
+}
--- samplefinaltest/src/com/finaltest/VariantCollection.java
+++ samplefinaltest/src/com/finaltest/VariantCollection.java
@@ -9,31 +9,38 @@ public class VariantCollection {
         variants = new CovidVariant[maxSize];
     }
 
-    // addVariant method
-    public void addVariant(CovidVariant v){
+    public int findIdx(String code){
         int left = 0;
         int right = currentSize-1;
         while(left<=right){
             int mid = (right+left)/2;
-            if (v.code.compareTo(variants[mid].code) == 0 ){
-                return;
+            if (code.compareTo(variants[mid].code) == 0 ){
+                return mid;
             }
-            else if (v.code.compareTo(variants[mid].code) > 0){
+            else if (code.compareTo(variants[mid].code) > 0){
                 left = mid+1;
             }
             else {
                 right = mid-1;
             }
         }
-        currentSize++;
-        variants[left] = v;
+        return left;
+    }
 
+    // addVariant method: insertion sort O(nlogn) for worst case -> O(logn) for average case
+    public void addVariant(CovidVariant v){
+        int idx = findIdx(v.code);
+        for (int i = currentSize-1; i>=idx; i--){
+            variants[i+1] = variants[i];
+        }
+        currentSize++;
+        variants[idx] = v;
     }
 
     // search method: average-case: O(logN)
     public CovidVariant search(String code){
         int left = 0;
-        int right = currentSize;
+        int right = currentSize-1;
 
         if (code == null || code.length() == 0){
             return null;
@@ -56,26 +63,30 @@ public class VariantCollection {
 
     // find previous variant method
     public CovidVariant previous(String code){
-        CovidVariant result = null;
-        return result;
+        int idx = findIdx(code);
+//        System.out.println(idx);
+        return variants[idx-1];
+    }
+
+    private void display(){
+        for (int i=0; i<currentSize;i++){
+            System.out.println("Variant " + i);
+            System.out.println(variants[i].name);
+            System.out.println(variants[i].code);
+        }
     }
 
     public static void main(String[] args){
         VariantCollection col = new VariantCollection();
         col.addVariant(new CovidVariant("Alpha", "210201A"));
-//        col.addVariant(new CovidVariant("Delta", "210311D"));
+        col.addVariant(new CovidVariant("Delta", "210311D"));
         col.addVariant(new CovidVariant("Omicron", "211120D"));
         col.addVariant(new CovidVariant("Beta", "210311A"));
-        System.out.println(col.variants[0]);
-        System.out.println(col.variants[1]);
-        System.out.println(col.variants[2]);
-        System.out.println(col.search("210311A")); // return the Beta variant
-//        col.previous("211120D"); // return the Delta variant
-//        for (int i=0; i<col.currentSize;i++){
-//            System.out.println("Variant " + i);
-//            System.out.println(col.variants[i].name);
-//            System.out.println(col.variants[i].code);
-//        }
+        col.addVariant(new CovidVariant("Gamma", "210511A"));
+        col.display();
+        System.out.println("Result found: "+col.search("211120D")); // return the Beta variant
+        System.out.println(col.previous("211120D")); // return the Delta variant
+
     }
 }
 
